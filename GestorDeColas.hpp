5a01a5c3fbf083dba03b0bedf1538c22d295a852
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-12-06 20:26:12 +0100

    Update GestorDeColas.hpp

--- GestorDeColas.hpp
+++ GestorDeColas.hpp
@@ -1,7 +1,14 @@
-#ifndef GESTOR_DE_COLAS.HPP
-#define GESTOR_DE_COLAS.HPP
-
- void getQueues();
+#ifndef GESTOR_DE_COLAS_HPP
+#define GESTOR_DE_COLAS_HPP
+#include <string>
+#include <iostream>
+using namespace std;
+const int NUM_COLAS = 3;
+struct tupla{
+    int palabra_clave[NUM_COLAS];
+    string nombre_cola[NUM_COLAS];
+};
+tupla getQueues();
 bool publish(int cola, string mensaje);
 bool read(int cola, string& mensaje);
 #endif
