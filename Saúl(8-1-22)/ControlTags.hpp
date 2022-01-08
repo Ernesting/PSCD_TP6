cb53475d23bafe455efe5992ad165006e2ea24a9
815743 <95444438+815743@users.noreply.github.com>
2022-01-08 18:50:09 +0100

    Add files via upload

---
+++ Saúl(8-1-22)/ControlTags.hpp
@@ -0,0 +1,22 @@
+#include <mutex>
+#include <condition_variable>
+#include "BoundedQueue.hpp"
+#include <string>
+using namespace std;
+
+
+class ControlTags{
+public:
+
+    ControlTags(const int n); 
+    void Getqueues();
+    void publish(string mensaje);
+    void read(string& mensaje);
+
+private:
+    int N;
+    BoundedQueue<string>* Tags;
+    mutex mtxMonitor;
+    condition_variable esperandolibre;
+    condition_variable esperandovacia;
+};
